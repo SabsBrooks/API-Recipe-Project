353eae955039a1391ade0b3db0c4d9509f2ba651
alicia <alicia.lecznar@gmail.com>
2019-08-20 12:05:38 -0400

    add to favorites. not complete

--- src/app/recipe.service.ts
+++ src/app/recipe.service.ts
@@ -1,15 +1,22 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { Observable } from 'rxjs';
+import { Observable } from "rxjs";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: "root"
 })
 export class RecipeService {
+  favorites: any[] = [];
 
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-    getFoodData(searchTerm: string): Observable<any> {
-        return this.http.get(`https://api.edamam.com/search?q=${searchTerm}&app_id=f2710e02&app_key=2ab4af09bde6752755d8fda5c38e6a3e`)
-    }
+  getFoodData(searchTerm: string): Observable<any> {
+    return this.http.get(
+      `https://api.edamam.com/search?q=${searchTerm}&app_id=f2710e02&app_key=2ab4af09bde6752755d8fda5c38e6a3e`
+    );
+  }
+  moveToFavorites(love: object) {
+    this.favorites.push(love);
+    console.log(this.favorites);
+  }
 }
